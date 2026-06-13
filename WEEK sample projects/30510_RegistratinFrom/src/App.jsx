b9a9a9f9e471f7bb3fa46c737a5037200ee98156
tejaswinitadukalateju <tejaswinitadukalateju@gmail.com>
2026-06-13 22:54:58 +0530

    Add files via upload

---
+++ WEEK sample projects/30510_RegistratinFrom/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RegistrationForm from "./components/RegistrationFrom.jsx";
+function App() {
+  return (
+    <div>
+      <h1>Student Registration System</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+export default App;
